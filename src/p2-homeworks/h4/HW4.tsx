d113b204b4720207f7c14e18fa74affc7494e83d
Oleh Vasylenko <muzmachine@gmail.com>
2022-02-10 22:32:29 +0200

    rr

--- src/p2-homeworks/h4/HW4.tsx
+++ src/p2-homeworks/h4/HW4.tsx
@@ -6,7 +6,7 @@ import SuperCheckbox from './common/c3-SuperCheckbox/SuperCheckbox'
 
 function HW4() {
     const [text, setText] = useState<string>('')
-    const error = text ? '' : 'error'
+    const error = text ? '' : 'errorrrrrrrrrrrrrrr'
 
     const showAlert = () => {
         if (error) {
